5c0ba24a2db4f66f9ed67d879ac6f8e20610fa4f
joaoconceicao12 <joao.m.n.conceicao@gmail.com>
2025-02-17 21:23:08 +0000

    reading files

--- editor.c
+++ editor.c
@@ -1,5 +1,9 @@
 /***includes ***/
 
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -166,15 +170,28 @@ int getWindowSize(int* rows, int* cols){
 
 /*** file i/o ***/
 
-void editorOpen(){
-  char *line = "Hello world!";
-  ssize_t linelen = 13;
+void editorOpen(char *filename){
+  FILE *fp = fopen(filename, "r");
+  if(!fp) die("fopen");
 
-  E.row.size = linelen;
-  E.row.chars = malloc(linelen + 1);
-  memcpy(E.row.chars, line, linelen);
-  E.row.chars[linelen] = '\0';
-  E.numrows = 1;
+  char *line = NULL;
+  size_t line_cap = 0;
+  ssize_t linelen;
+  linelen = getline(&line, &line_cap, fp);
+  if(linelen != -1){
+    //tirar \n e \r
+    while(linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')){
+      linelen--;
+    }
+    E.row.size = linelen;
+    E.row.chars = malloc(linelen + 1);
+    memcpy(E.row.chars, line, linelen);
+    E.row.chars[linelen] = '\0';
+    E.numrows = 1;
+  }
+  free(line);
+  fclose(fp);
+
 }
 
 
@@ -341,13 +358,13 @@ void initEditor(){
 }
 
 
-int main(){
-    //printf("passei");
-    //write(STDOUT_FILENO, "passei", 6);
-    //fflush(stdout);
+int main(int argc, char* argv[]){
+
     enableRawMode();
     initEditor();
-    editorOpen();
+    if(argc >= 2){
+      editorOpen(argv[1]);
+    }
     
     while(1){
         editorRefreshScreen();
