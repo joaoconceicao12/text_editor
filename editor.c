ad609a2fb483a76146d4c96c30e56a2ce38d5b98
joaoconceicao12 <joao.m.n.conceicao@gmail.com>
2025-02-18 21:05:14 +0000

    render tabs

--- editor.c
+++ editor.c
@@ -18,13 +18,15 @@
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define KILO_VERSION "0.0.1"
-
+#define KILO_TAB_STOP 8
 
 /*** data ***/
 
 typedef struct erow {
   int size;
+  int rsize;
   char *chars;
+  char* render;
 } erow;
 
 struct editorConfig{
@@ -71,6 +73,14 @@ void editorRefreshScreen();
 void editorDrawRows(struct abuf *ab);
 void editorProcessKeypress();
 int getCursorPosition(int *rows, int *cols);
+int getWindowSize(int* rows, int* cols);
+void editorAppendRow(char *s, size_t len);
+void editorOpen(char *filename);
+void abAppend(struct abuf *ab, const char *s, int len);
+void abFree(struct abuf *ab);
+void editorScroll();
+void editorMoveCursor(int key);
+void initEditor();
 
 /*** terminal ***/
 
@@ -172,6 +182,27 @@ int getWindowSize(int* rows, int* cols){
 
 /*** row operations ***/
 
+void editorUpdateRow(erow *row){
+  int tabs = 0;
+  int j;
+  for (j = 0; j < row->size; j++)
+    if (row->chars[j] == '\t') tabs++;
+  free(row->render);
+  row->render = malloc(row->size + tabs*(KILO_TAB_STOP - 1) + 1);
+  int idx = 0;
+  for (j = 0; j < row->size; j++) {
+    if (row->chars[j] == '\t') {
+      row->render[idx++] = ' ';
+      while (idx % KILO_TAB_STOP != 0) row->render[idx++] = ' ';
+    } else {
+      row->render[idx++] = row->chars[j];
+    }
+  }
+  row->render[idx] = '\0';
+  row->rsize = idx;
+}
+
+
 void editorAppendRow(char *s, size_t len){
   E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
 
@@ -180,6 +211,11 @@ void editorAppendRow(char *s, size_t len){
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
   E.row[at].chars[len] = '\0';
+
+  E.row->rsize = 0;
+  E.row->render = NULL;
+  editorUpdateRow(&E.row[at]);
+
   E.numrows++;
 }
 
@@ -282,10 +318,10 @@ void editorDrawRows(struct abuf *ab) {
           abAppend(ab, "~", 1);
         }
       }else{
-        int len = E.row[filerow].size - E.coloff;
+        int len = E.row[filerow].rsize - E.coloff;
         if(len < 0) len = 0;
         if(len > E.screen_cols) len = E.screen_cols;
-        abAppend(ab, &E.row[filerow].chars[E.coloff], len);
+        abAppend(ab, &E.row[filerow].render[E.coloff], len);
       }
       abAppend(ab, "\x1b[K", 3);//limpar uma linha
       if(y < E.screen_rows - 1){
