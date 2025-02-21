018823db0e0f5984fc7edb48ed387095b539ba2c
joaoconceicao12 <joao.m.n.conceicao@gmail.com>
2025-02-21 13:28:15 +0000

    Backspacing at start of line

--- editor.c
+++ editor.c
@@ -283,6 +283,31 @@ void editorRowDelChar(erow *row, int at){
   E.dirty++;
 }
 
+void editorFreeRow(erow *row){
+  free(row->render);
+  free(row->chars);
+}
+
+void editorDelRow(int at){
+  if(at < 0 || at >= E.numrows) return; //checks if cursor is on a file line
+
+  editorFreeRow(&E.row[at]);
+  memmove(&E.row[at], &E.row[at + 1], sizeof(erow) * E.numrows - at -1); 
+  //we overwrite the row we are deleting
+  //-1 represents the row we are deleting
+  E.numrows--;
+  E.dirty++;
+}
+
+void editorRowAppendString(erow *row, char *s, size_t len){
+  row->chars = realloc(row->chars, row->size + len + 1);
+  memcpy(&row->chars[row->size], s, len);
+  row->size += len;
+  row->chars[row->size] = '\0';
+  editorUpdateRow(row);
+  E.dirty++;
+}
+
 /*** editor operations ***/
 
 void editorInsertChar(int c){
@@ -296,11 +321,17 @@ void editorInsertChar(int c){
 
 void editorDelChar(){
   if(E.cy == E.numrows) return; //In case the cursor is past EOF
+  if(E.cx == 0 && E.cy == 0) return;
 
   erow *row = &E.row[E.cy];
-  if(E.cx > 0){ //In case the cursor is in the start of the line
+  if(E.cx > 0){ //In case the cursor is not the start of the line
     editorRowDelChar(row, E.cx);
     E.cx--;
+  }else{
+    E.cx = E.row[E.cy - 1].size;
+    editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
+    editorDelRow(E.cy);
+    E.cy--;
   }
 }
 
