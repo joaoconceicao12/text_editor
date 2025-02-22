cb20aaf3ca626796a9d638ef97e9150e040f531d
joaoconceicao12 <joao.m.n.conceicao@gmail.com>
2025-02-22 06:04:37 +0000

    save as function added

--- editor.c
+++ editor.c
@@ -1,4 +1,3 @@
-
 /*** includes ***/
 
 #define _DEFAULT_SOURCE
@@ -89,10 +88,12 @@ void editorProcessKeypress();
 void editorMoveCursor(int key);
 int getCursorPosition(int *rows, int *cols);
 int getWindowSize(int* rows, int* cols);
-void editorAppendRow(char *s, size_t len);
+void editorInsertRow(int at, char *s, size_t len);
 void editorUpdateRow(erow *row);
 void editorRowInsertChar(erow *row, int at, int c);
 void editorInsertChar(int c);
+void editorInsertNewline();
+void editorDelChar();
 char *editorRowsToString(int *buflen);
 void editorOpen(char *filename);
 void editorSave();
@@ -100,6 +101,11 @@ void abAppend(struct abuf *ab, const char *s, int len);
 void abFree(struct abuf *ab);
 void editorScroll();
 void initEditor();
+void editorFreeRow(erow *row);
+void editorDelRow(int at);
+void editorRowAppendString(erow *row, char *s, size_t len);
+int editorRowCxToRx(erow *row, int cx);
+char* editorPrompt(char* prompt);
 
 /*** terminal ***/
 
@@ -245,10 +251,13 @@ void editorUpdateRow(erow *row){
 }
 
 
-void editorAppendRow(char *s, size_t len){
+void editorInsertRow(int at, char *s, size_t len){
+  if(at < 0 || at >= E.numrows) return;
+  
+  
   E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+  memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
 
-  int at = E.numrows;
   E.row[at].size = len;
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
@@ -313,10 +322,25 @@ void editorRowAppendString(erow *row, char *s, size_t len){
 void editorInsertChar(int c){
   
   if(E.cy == E.numrows){ //the cursor is on the tilde line after EOF
-    editorAppendRow("", 0); //we append a new line so we can write on it
+    editorInsertRow(E.numrows, "", 0); //we append a new line so we can write on it
   }
   editorRowInsertChar(&E.row[E.cy], E.cx, c);
   E.cx++;
+}
+
+void editorInsertNewline(){
+  if(E.cx == 0){
+    editorInsertRow(E.cy, "", 0);
+  }else{
+    erow *row = &E.row[E.cy];
+    editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+    row = &E.row[E.cy];
+    row->size = E.cx;
+    row->chars[row->size] = '\0';
+    editorUpdateRow(row);
+  }
+  E.cy++;
+  E.cx = 0;
 }
 
 void editorDelChar(){
@@ -376,7 +400,7 @@ void editorOpen(char *filename){
     while(linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r')){
       linelen--;
     }
-    editorAppendRow(line, linelen);
+    editorInsertRow(E.numrows, line, linelen);
   }
   free(line);
   fclose(fp);
@@ -386,7 +410,13 @@ void editorOpen(char *filename){
 
 void editorSave(){
 
-  if(E.filename == NULL) return;
+  if(E.filename == NULL){
+    E.filename = editorPrompt("Save as: %s");
+    if(E.filename == NULL){
+      editorSetStatusMessage("Save aborted");
+      return;
+    }
+  }
 
   int len;
   char *buf = editorRowsToString(&len); //buf's malloc done in func
@@ -572,6 +602,42 @@ void editorDrawRows(struct abuf *ab) {
 
 /*** input ***/
 
+char* editorPrompt(char* prompt){
+  size_t bufsize = 128;
+  char *buf = malloc(bufsize);
+
+  size_t buflen = 0;
+  buf[0] = '\0';
+
+  while(1){
+    editorSetStatusMessage(prompt, buf);
+    editorRefreshScreen();
+
+    int c = editorReadKey();
+
+    if(c == DEL_KEY || c == CTRL_KEY('h') || c == BACKSPACE){
+      if(buflen != 0) buf[--buflen] = '\0';
+    }else if(c == '\x1b'){
+      editorSetStatusMessage("");
+      free(buf);
+      return NULL;
+    }else if(c == '\r'){
+      if(buflen != 0){
+        editorSetStatusMessage("");
+        return buf;
+      }
+    }else if(!iscntrl(c) && c < 128){
+      if(buflen == bufsize - 1){
+        bufsize *= 2;
+        buf = realloc(buf,bufsize);
+      }
+      buf[buflen++] = c;
+      buf[buflen] = '\0';
+    }
+  }
+}
+
+
 void editorMoveCursor(int key) {
 
   erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
@@ -636,7 +702,7 @@ void editorProcessKeypress() {
 
     switch (c) {
       case 'r':
-        //TODO
+        editorInsertNewline();
         break;
       
       case CTRL_KEY('q'):
